b32626dcc58ae0898d2f75f89cf9a82aa3eee185
Joe <jonathanbonette@hotmail.com>
2025-07-27 05:18:53 -0300

    removing unneessary files from folder

--- freeRTOS/borracho/html/search/all_11.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['xcontrolqueue_0',['xControlQueue',['../main_8cpp.html#a4a467a81ab512fc4a226ef182b8e2196',1,'main.cpp']]],
-  ['xdisplayqueue_1',['xDisplayQueue',['../main_8cpp.html#a51e8f735c66d2f4634819af0c5a43301',1,'main.cpp']]],
-  ['xkeypadqueue_2',['xKeypadQueue',['../main_8cpp.html#aa63b118af57eeb18597abe006808610d',1,'main.cpp']]],
-  ['xsensorqueue_3',['xSensorQueue',['../main_8cpp.html#a0927c92b68493a1c4ad301b66b6eef1b',1,'main.cpp']]]
-];
